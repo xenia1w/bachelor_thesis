78309ae43c6d2b6b06ec8f8cb9923c144d051672
xenia1w <xenia.wysokinska@gmail.com>
2024-02-21 14:54:41 +0100

    corrected to the form presented in the thesis

--- api_prompt.py
+++ api_prompt.py
@@ -1,12 +1,20 @@
-import os
 import openai
 
-openai.api_key = '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'
+# add here your personal API key that can be obtained from: https://platform.openai.com/api-keys
+# following the best practice it is recommended to hide the actual key in a separate file
+personal_api_key = ""
+
+openai.api_key = personal_api_key
 
 response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": "AI Medical assistant helping a patient to fill in a survey. When the patient gives an answer do a general affirm their answer." }
+        {"role": "system",
+         "content": "<your_text_here>"},
+        {"role": "assistant",
+         "content": "<your_text_here>"},
+        {"role": "user",
+         "content": "<your_text_here>"}
     ],
     max_tokens=100
 )
