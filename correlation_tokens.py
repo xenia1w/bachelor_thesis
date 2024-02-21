e5b1f92881b8e0ade6f5a54e4be2500f643a5c07
xenia1w <xenia.wysokinska@gmail.com>
2024-02-21 14:01:19 +0100

    added spreadsheets and comments

--- correlation_tokens.py
+++ correlation_tokens.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import numpy as np
 
-excel_path = '/Users/xmena/Documents/TU/BachelorThesis/survey_results_tokens.xlsx'
+excel_path = 'survey_results.xlsx'
 all_prompts = ['s', 'ui', 'nsi', 'op', 'eng', 'end']
 eval_criteria = ['correctness', 'friendliness', 'ease_of_reading', 'length']
 
 columns_to_read = []
 
+# Specifying all the columns that should be extracted from the excel file
 for prompt in all_prompts:
     for i in range(1, 4):
         for criteria in eval_criteria:
@@ -16,6 +16,9 @@ for prompt in all_prompts:
             columns_to_read.append(column)
 
 df = pd.read_excel(excel_path, sheet_name='w_tokens', usecols=columns_to_read, nrows=59)
+
+# creating a main and empty DataFrame with four columns of the features for which we would like to plot the
+# correlation matrix
 df_corr = pd.DataFrame(columns=['correctness', 'friendliness', 'ease_of_reading', 'output_tokens'])
 
 for prompt in all_prompts:
@@ -28,11 +31,13 @@ for prompt in all_prompts:
         new_df = df[[correctness, friendliness, ease_of_reading, output_tokens]].copy()
         new_df.columns = ['correctness', 'friendliness', 'ease_of_reading', 'output_tokens']
 
+        # attach the specified columns to the main DataFrame (df_corr)
         df_corr = pd.concat([df_corr, new_df])
 
+# calculate the correlation matrix
 correlation_matrix = df_corr.corr()
 
-# Plot the correlation matrix as a heatmap
+# plot the correlation matrix as a heatmap
 plt.figure(figsize=(8, 6))
 sns.heatmap(correlation_matrix, annot=True, cmap="crest", fmt=".2f")
 plt.title('Correlation Matrix')
--- main.py
+++
@@ -1,8 +0,0 @@
-import openai
-
-openai.api_key = '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'
-
-prompt = 'Say hello'
-response = openai.Completion.create(engine="davinci-003", prompt=prompt, max_tokens=40)
-
-print(response)
